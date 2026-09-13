ff7164f6ca59112ea9f36447ee53d786a8ff3bf7
Shirin <deliverioshirin@gmail.com>
2026-09-13 19:58:46 +0800

    Fix API routing for produxtion

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? '/api' : '',
   withCredentials: true,
   timeout: 10000
 })
